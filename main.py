55d01851cf157d782c9991e6968bd9fca220ecad
Ed7723 <linde2222@hotmail.com>
2023-04-01 21:42:39 -0400

    Created and updated HTML templates

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html", title = 'Homepage')
+
+@app.route("/create")
+def create():
+    return render_template("create.html", title = 'Create Items')
+
+@app.route("/view")
+def view():
+    return render_template("view.html", title = 'View Items')
